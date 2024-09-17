06135db638f1beffb2269d2f19d788ac7906a879
Sara <sape0003@stud.kea.dk>
2024-09-17 10:29:52 +0200

    Major changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
         "light-green": "#798E7F",
         "dark-green": "#23483B",
         "off-white": "#F7F7F5",
+        "light-yellow": "#FFBB00",
+        "medium-yellow": "#FFCB3F",
+        "dark-yellow": "#C89300",
       },
       plugins: [
         require("@tailwindcss/typography"),
